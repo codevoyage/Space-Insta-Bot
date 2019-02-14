170328332e8a694eed7c2656a092aad53e0bd2ec
shikha <shimahaj@adobe.com>
2019-02-13 20:20:38 -0500

    cofigured to post a picture daily

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import schedule
 from functions import post_picture_insta
 
-schedule.every(30).seconds.do(post_picture_insta)
+schedule.every().day.do(post_picture_insta)
 
 while True:
     schedule.run_pending()
