01e7b3bceb051061efeb7bb576d86f5f1fcaea84
shikha <shimahaj@adobe.com>
2019-02-17 09:14:23 -0500

    changed time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import schedule
 from functions import post_picture_insta
 
-schedule.every().day.at("8:00").do(post_picture_insta)
+schedule.every().day.at("09:15").do(post_picture_insta)
 
 while True:
     schedule.run_pending()
